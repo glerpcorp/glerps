b48bdb80649a792070e0fc8525b84ac32a700441
Conor Hanrahan <conorhanrahan@Conors-MacBook-Pro.local>
2026-02-16 22:50:30 -0800

    add linting and remove junk

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,34 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    ...nextVitals,
+    ...nextTs,
+    prettierConfig,
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'max-len': ['error', { code: 90, tabWidth: 4 }],
+            'no-console': ['error'],
+            'no-multi-spaces': ['error'],
+            quotes: ['error', 'single'],
+            semi: ['error', 'never'],
+        },
+    },
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+    ]),
+])
 
-export default eslintConfig;
+export default eslintConfig
